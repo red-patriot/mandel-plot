a4f47e959392ad89e939b4327804ab51be19f693
Bryson Tanner <bltanner105@gmail.com>
2024-09-29 13:45:01 -0500

    docs: Update documentation for FractalView

--- include/fractal_view.hpp
+++ include/fractal_view.hpp
@@ -6,30 +6,37 @@
 #include <SDL2/SDL.h>
 
 namespace mandel {
+  /** A view window for an iterative Fractal */
   class FractalView {
    public:
     FractalView(int width, int height);
     ~FractalView();
 
+    /**
+     * Initializes a FractalView.
+     *
+     * \param width The witdh of the viewport to display
+     * \param height The height of the viewport to display
+     *
+     * \throw std::runtime_error if there is already an instance created (there can only be one allowed at a time)
+     */
     static FractalView* init(int width, int height);
 
+    /** Shows the plot */
     void showPlot();
 
    private:
+    static std::unique_ptr<FractalView> instance_; /**< The unique view instance */
 
-    static std::unique_ptr<FractalView> instance_;
-
-    int width_;
-    int height_;
-    SDL_Window* window_{nullptr};
-    SDL_Renderer* renderer_{nullptr};
-
+    int width_;                       /**< The width of the viewport*/
+    int height_;                      /**< The height of the viewport*/
+    SDL_Window* window_{nullptr};     /**< The SDL window to draw to*/
+    SDL_Renderer* renderer_{nullptr}; /**< The Renderer to use to display the window contents*/
 
     FractalView(const FractalView&) = delete;
     FractalView& operator=(const FractalView&) = delete;
     FractalView(FractalView&&) = delete;
     FractalView& operator=(FractalView&&) = delete;
-
   };
 }  // namespace mandel
 
