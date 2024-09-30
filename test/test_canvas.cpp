f94e75352b6db9689d807b7d153298dd0b851491
Bryson Tanner <bltanner105@gmail.com>
2024-09-29 21:09:36 -0500

    test: Add basic functionality for Canvas interactions

--- test/test_canvas.cpp
+++ test/test_canvas.cpp
@@ -1,36 +1,38 @@
+#include <algorithm>
+
 #include <Eigen/Core>
 #include <gtest/gtest.h>
 
 #include "canvas.hpp"
 
-using Eigen::Vector2d;
+using namespace std::complex_literals;
 
 TEST(TestCanvas, Has2DCanvasField) {
-  Vector2d smallestPoint{-1.0, -1.0};
-  Vector2d greatestPoint{1.0, 1.0};
+  plot::Canvas::Point smallestPoint{-1.0, -1.0};
+  plot::Canvas::Point greatestPoint{1.0, 1.0};
 
   plot::Canvas uut{smallestPoint, greatestPoint, 100, 100};
 
-  auto& actual = uut.getPoints();
+  auto& actual = uut.allPoints();
 
   EXPECT_EQ(100 * 100, actual.size());
 }
 
 TEST(TestCanvas, ConstructsCanvasOfCorrectSize) {
-  Vector2d smallestPoint{-1.0, -1.0};
-  Vector2d greatestPoint{1.0, 1.0};
+  plot::Canvas::Point smallestPoint{-1.0, -1.0};
+  plot::Canvas::Point greatestPoint{1.0, 1.0};
 
   plot::Canvas uut{smallestPoint, greatestPoint, 50, 100};
 
-  auto& actual = uut.getPoints();
+  auto& actual = uut.allPoints();
 
   EXPECT_EQ(50, actual.cols());
   EXPECT_EQ(100, actual.rows());
 }
 
 TEST(TestCanvas, GetWidth) {
-  Vector2d smallestPoint{-1.0, -1.0};
-  Vector2d greatestPoint{1.0, 1.0};
+  plot::Canvas::Point smallestPoint{-1.0, -1.0};
+  plot::Canvas::Point greatestPoint{1.0, 1.0};
 
   plot::Canvas uut{smallestPoint, greatestPoint, 1580, 1960};
 
@@ -38,8 +40,8 @@ TEST(TestCanvas, GetWidth) {
 }
 
 TEST(TestCanvas, GetHeight) {
-  Vector2d smallestPoint{-1.0, -1.0};
-  Vector2d greatestPoint{1.0, 1.0};
+  plot::Canvas::Point smallestPoint{-1.0, -1.0};
+  plot::Canvas::Point greatestPoint{1.0, 1.0};
 
   plot::Canvas uut{smallestPoint, greatestPoint, 1580, 1960};
 
