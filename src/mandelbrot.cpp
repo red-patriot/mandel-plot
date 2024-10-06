3aca0cac64e417d9290796318eae3bc5ec851bdf
Bryson Tanner <bltanner105@gmail.com>
2024-10-06 10:38:14 -0500

    feat: Implement basic equations for calculating mandelbrot iterations

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -4,4 +4,18 @@ namespace mandelbrot {
   std::complex<double> f(std::complex<double> z_n, std::complex<double> c) noexcept {
     return (z_n * z_n) + c;
   }
+
+  size_t escapeTime(std::complex<double> c, size_t limit) {
+    static constexpr double THRESHOLD = 2.0;
+
+    std::complex<double> z = 0.0;
+    for (size_t iteration = 1; iteration <= limit; ++iteration) {
+      z = f(z, c);
+      if (std::abs(z) > THRESHOLD) {
+        return iteration;
+      }
+    }
+
+    return limit;
+  }
 }  // namespace mandelbrot
--- test/test_mandelbrot_equation.cpp
+++ test/test_mandelbrot_equation.cpp
@@ -9,8 +9,8 @@ using std::operator""i;
 using std::tuple;
 
 class TestMandelbrotEquation : public ::testing::TestWithParam<tuple<std::complex<double>,
-                                                             std::complex<double>,
-                                                             std::complex<double>>> {
+                                                                     std::complex<double>,
+                                                                     std::complex<double>>> {
 };
 
 TEST_P(TestMandelbrotEquation, Test) {
@@ -22,17 +22,35 @@ TEST_P(TestMandelbrotEquation, Test) {
   EXPECT_NEAR(expected.imag(), actual.imag(), 1e-6);
 }
 
-INSTANTIATE_TEST_CASE_P(, TestMandelbrotEquation,
-                        ::testing::Values(tuple{2i,
-                                                1.0 + 1i,
-                                                0i},
-                                          tuple{
-                                              7.0132 - 0.2176i,
-                                              0.34 - 0.32i,
-                                              7.0 + 0i},
-                                          tuple{83.838433 - 27.196576i,
-                                                9.32143 - 1.432i,
-                                                -1.0 - 0.5i},
-                                          tuple{-1.35 - 0.72i,
-                                                0.3 - 1.2i,
-                                                0i}));
+INSTANTIATE_TEST_SUITE_P(, TestMandelbrotEquation,
+                         ::testing::Values(tuple{2i,
+                                                 1.0 + 1i,
+                                                 0i},
+                                           tuple{
+                                               7.0132 - 0.2176i,
+                                               0.34 - 0.32i,
+                                               7.0 + 0i},
+                                           tuple{83.838433 - 27.196576i,
+                                                 9.32143 - 1.432i,
+                                                 -1.0 - 0.5i},
+                                           tuple{-1.35 - 0.72i,
+                                                 0.3 - 1.2i,
+                                                 0i}));
+
+class TestMandelbrotEscape : public ::testing::TestWithParam<tuple<size_t,
+                                                                   std::complex<double>>> {
+};
+
+TEST_P(TestMandelbrotEscape, Test) {
+  auto& [expected, point] = GetParam();
+
+  auto actual = mandelbrot::escapeTime(point, 1000);
+
+  EXPECT_EQ(expected, actual);
+}
+
+INSTANTIATE_TEST_SUITE_P(, TestMandelbrotEscape,
+                         ::testing::Values(tuple{1, 2.000001 + 0i},
+                                           tuple{2, 2.0i},
+                                           tuple{1000, -1.0 + 0i},
+                                           tuple{3, 0.5 + 0.75i}));
