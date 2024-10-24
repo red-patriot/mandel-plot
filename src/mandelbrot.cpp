68aae5295a25920869504533ba0aeaa77401c11a
Bryson Tanner <bltanner105@gmail.com>
2024-10-24 17:01:24 -0500

    feat: Use abs2 instead of abs to determine escape of points

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -1,18 +1,22 @@
 #include "mandelbrot.hpp"
 
 namespace mandelbrot {
+  static double abs2(std::complex<double> z) {
+    return z.real() * z.real() + z.imag() * z.imag();
+  }
+
   std::complex<double> f(std::complex<double> z_n, std::complex<double> c) noexcept {
     return (z_n * z_n) + c;
   }
 
   plot::Escape escapeTime(std::complex<double> c, size_t limit) {
-    static constexpr double THRESHOLD = plot::ESCAPE_RADIUS;
+    static constexpr double THRESHOLD = plot::ESCAPE_RADIUS * plot::ESCAPE_RADIUS;
     plot::Escape ret;
 
     ret.value = 0.0;
     for (ret.iteration = 1; ret.iteration <= limit; ++ret.iteration) {
       ret.value = f(ret.value, c);
-      if (std::abs(ret.value) > THRESHOLD) {
+      if (abs2(ret.value) > THRESHOLD) {
         return ret;
       }
     }
