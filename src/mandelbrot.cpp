e3c608af68e35136e588262ba101a831bdbd2501
Bryson Tanner <bltanner105@gmail.com>
2024-10-26 13:44:18 -0500

    perf: Add cardioid or P2 bulb checking to not iterate on most points.

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -1,5 +1,15 @@
 #include "mandelbrot.hpp"
 
+namespace {
+  bool isInMainCardioidOrMainBulb(std::complex<double> z) {
+    double x = z.real();
+    double y = z.imag();
+    double q = (x - 0.25) * (x - 0.25) + y * y;
+    return q * (q + (x - 0.25)) <= 0.25 * y * y ||
+           (x + 1.0) * (x + 1.0) + y * y <= 0.0625;
+  }
+}  // namespace
+
 namespace mandelbrot {
   static double abs2(std::complex<double> z) {
     return z.real() * z.real() + z.imag() * z.imag();
@@ -10,17 +20,22 @@ namespace mandelbrot {
   }
 
   plot::Escape escapeTime(std::complex<double> c, size_t limit) {
-    static constexpr double THRESHOLD = plot::ESCAPE_RADIUS * plot::ESCAPE_RADIUS;
-    plot::Escape ret;
+    plot::Escape result{plot::DOES_NOT_ESCAPE, c};
+    if (::isInMainCardioidOrMainBulb(c)) {
+      return result;
+    }
 
-    ret.value = 0.0;
-    for (ret.iteration = 1; ret.iteration <= limit; ++ret.iteration) {
-      ret.value = f(ret.value, c);
-      if (abs2(ret.value) > THRESHOLD) {
-        return ret;
+    static constexpr double THRESHOLD = plot::ESCAPE_RADIUS * plot::ESCAPE_RADIUS;
+
+    result.value = 0.0;
+    for (result.iteration = 1; result.iteration <= limit; ++result.iteration) {
+      result.value = f(result.value, c);
+      if (abs2(result.value) > THRESHOLD) {
+        return result;
       }
     }
-    ret.iteration = plot::DOES_NOT_ESCAPE;
-    return ret;
+
+    result.iteration = plot::DOES_NOT_ESCAPE;
+    return result;
   }
 }  // namespace mandelbrot
