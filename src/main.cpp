3328687de9c6e7a7905e0f1112c59e0a2aac4ca8
Bryson Tanner <bltanner105@gmail.com>
2024-11-23 08:54:17 -0600

    chore: Remove drawing code, for now, to add cli arg parsing

--- src/main.cpp
+++ src/main.cpp
@@ -13,25 +13,27 @@ using namespace std::chrono_literals;
 using std::operator""i;
 
 int main() {
-  auto canvas = std::make_shared<plot::Canvas>(-2.5 + 1.5i,
-                                               1.0 - 1.5i,
-                                               1108, 950);
-  std::unique_ptr<plot::ColorCalculator> calculator =
-      std::make_unique<plot::ParallelCalculator>(8,
-                                                 std::vector<plot::Color>{0x33'CE'FF'FF,
-                                                                          0x33'68'FF'FF,
-                                                                          0x64'33'FF'FF,
-                                                                          0xCA'33'FF'FF,
-                                                                          0xFF'33'CE'FF,
-                                                                          0xFF'33'68'FF},
-                                                 plot::BLACK,
-                                                 mandelbrot::escapeTime,
-                                                 canvas);
+   // TODO: Parse arguments
 
-  auto view = plot::FractalView::init(std::move(canvas),
-                                      std::move(calculator));
+  //auto canvas = std::make_shared<plot::Canvas>(-2.5 + 1.5i,
+  //                                             1.0 - 1.5i,
+  //                                             1108, 950);
+  //std::unique_ptr<plot::ColorCalculator> calculator =
+  //    std::make_unique<plot::ParallelCalculator>(8,
+  //                                               std::vector<plot::Color>{0x33'CE'FF'FF,
+  //                                                                        0x33'68'FF'FF,
+  //                                                                        0x64'33'FF'FF,
+  //                                                                        0xCA'33'FF'FF,
+  //                                                                        0xFF'33'CE'FF,
+  //                                                                        0xFF'33'68'FF},
+  //                                               plot::BLACK,
+  //                                               mandelbrot::escapeTime,
+  //                                               canvas);
 
-  view->showPlot();
+  //auto view = plot::FractalView::init(std::move(canvas),
+  //                                    std::move(calculator));
+
+  //view->showPlot();
 
   return 0;
 }
