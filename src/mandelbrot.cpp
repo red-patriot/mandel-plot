cb1de3c2f6d3b59c6aeba12d63a66df9bbfbca81
Bryson Tanner <bltanner105@gmail.com>
2024-11-23 08:30:21 -0600

    feat: Check the periodicity of points in the set up to cycles 20 long

--- src/mandelbrot.cpp
+++ src/mandelbrot.cpp
@@ -8,12 +8,17 @@ namespace {
     return q * (q + (x - 0.25)) <= 0.25 * y * y ||
            (x + 1.0) * (x + 1.0) + y * y <= 0.0625;
   }
+  double abs2(std::complex<double> z) {
+    return z.real() * z.real() + z.imag() * z.imag();
+  }
+  bool approx(const std::complex<double>& a, const std::complex<double>& b) {
+    static constexpr double THRESHOLD = 1e-12;
+    return std::abs(a.real() - b.real()) < THRESHOLD &&
+           std::abs(a.imag() - b.imag()) < THRESHOLD;
+  }
 }  // namespace
 
 namespace mandelbrot {
-  static double abs2(std::complex<double> z) {
-    return z.real() * z.real() + z.imag() * z.imag();
-  }
 
   std::complex<double> f(std::complex<double> z_n, std::complex<double> c) noexcept {
     return (z_n * z_n) + c;
@@ -26,12 +31,22 @@ namespace mandelbrot {
     }
 
     static constexpr double THRESHOLD = plot::ESCAPE_RADIUS * plot::ESCAPE_RADIUS;
+    static constexpr size_t MAX_PERIOD = 20;
 
     result.value = 0.0;
+    std::complex<double> oldValue = result.value;
     for (result.iteration = 1; result.iteration <= limit; ++result.iteration) {
       result.value = f(result.value, c);
-      if (abs2(result.value) > THRESHOLD) {
+      if (::abs2(result.value) > THRESHOLD) {
         return result;
+      }
+      // check periodicity
+      if (::approx(result.value, oldValue)) {
+        break;
+      }
+
+      if (result.iteration % MAX_PERIOD == 0) {
+        oldValue = result.value;
       }
     }
 
